778ea1ea62923c9ef12dec72478a1c37cef0ee94
Rafadcosta <rafaeladuqu@gmail.com>
2020-07-30 23:32:06 -0300

    setando jogadas

--- js/jogo.js
+++ js/jogo.js
@@ -1,3 +1,6 @@
+var rodada = 1;
+var matriz_jogo = Array(3);
+
 $(document).ready( function (){
 
     $('#btn_play').click ( function () {
@@ -14,6 +17,31 @@ $(document).ready( function (){
 
         $('#nick_1').html($('#nick1').val());
         $('#nick_2').html($('#nick2').val());
+
+        $('#home').hide();
+        $('#game').show();
     });
+
+    $('.move').click(function(){
+        var id_campo = this.id;
+        move(id_campo);
+    });
+
+    function move(id){
+        var icone = '';
+        var ponto = 0;
+
+        if((rodada % 2 == 1)){
+            icone = 'url("../images/m1.png")';
+            ponto = -1;
+        } else {
+            icone = 'url("../images/m2.png")';
+            ponto = 1;
+        }
+
+        rodada++;
+
+        $('#'+id).css('background-image',icone);
+    }
 
 });
