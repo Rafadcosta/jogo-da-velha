d9e20acde95a60d71672d1d2122b00da81e179f7
Rafadcosta <rafaeladuqu@gmail.com>
2020-07-30 23:59:57 -0300

    finish functions

--- js/jogo.js
+++ js/jogo.js
@@ -1,6 +1,22 @@
 var rodada = 1;
 var matriz_jogo = Array(3);
 
+matriz_jogo['a'] = Array(3);
+matriz_jogo['b'] = Array(3);
+matriz_jogo['c'] = Array(3);
+
+matriz_jogo['a'][1] = 0;
+matriz_jogo['a'][2] = 0;
+matriz_jogo['a'][3] = 0;
+
+matriz_jogo['b'][1] = 0;
+matriz_jogo['b'][2] = 0;
+matriz_jogo['b'][3] = 0;
+
+matriz_jogo['c'][1] = 0;
+matriz_jogo['c'][2] = 0;
+matriz_jogo['c'][3] = 0;
+
 $(document).ready( function (){
 
     $('#btn_play').click ( function () {
@@ -23,8 +39,9 @@ $(document).ready( function (){
     });
 
     $('.move').click(function(){
-        var id_campo = this.id;
-        move(id_campo);
+        var id_field = this.id;
+        $('#'+id_field).off();
+        move(id_field);
     });
 
     function move(id){
@@ -42,6 +59,72 @@ $(document).ready( function (){
         rodada++;
 
         $('#'+id).css('background-image',icone);
+
+        var linha_coluna = id.split('-');
+
+        matriz_jogo[linha_coluna[0]][linha_coluna[1]] = ponto;
+
+        check();
+    }
+
+    function check(){
+        // horizontal
+        var pontos = 0;
+        for(var i=1;i<=3;i++){
+            pontos = pontos + matriz_jogo['a'][i];
+        }
+
+        winner(pontos);
+        pontos = 0;
+
+        for(var i=1;i<=3;i++){
+            pontos = pontos + matriz_jogo['b'][i];
+        }
+
+        winner(pontos);
+        pontos = 0;
+        
+        for(var i=1;i<=3;i++){
+            pontos = pontos + matriz_jogo['c'][i];
+        }
+
+        winner(pontos);
+        
+        // vertical
+        for(var l=1;l<=3;l++){
+            pontos = 0;
+
+            pontos += matriz_jogo['a'][l];
+            pontos += matriz_jogo['b'][l];
+            pontos += matriz_jogo['c'][l];
+
+            winner(pontos);
+        }
+
+        // diagonal
+        pontos = 0;
+        pontos = matriz_jogo['a'][1] + matriz_jogo['b'][2] + matriz_jogo['c'][3];
+        winner(pontos);
+
+        pontos = 0;
+        pontos = matriz_jogo['a'][3] + matriz_jogo['b'][2] + matriz_jogo['c'][1];
+
+        winner(pontos);
+
+    }
+
+    function winner(pontos){
+        if(pontos == -3){
+            var move_1 = $('#nick1').val();
+            alert(move_1 + ' venceu!');
+            $('.jogada').off();
+        }
+
+        else if(pontos == 3){
+            var move_2 = $('#nick2').val();
+            alert(move_2 + ' venceu!');
+            $('.jogada').off();
+        }
     }
 
 });
